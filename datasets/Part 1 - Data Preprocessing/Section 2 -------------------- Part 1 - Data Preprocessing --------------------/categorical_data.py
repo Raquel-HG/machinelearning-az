1f255bd62640d892ccb925f33ecb3de48385bc61
Raquel-HG <62893716+Raquel-HG@users.noreply.github.com>
2020-04-10 18:47:19 +0200

    Update categorical_data.py

--- datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/categorical_data.py
+++ datasets/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/categorical_data.py
@@ -42,30 +42,13 @@ X = np.array(ct.fit_transform(X), dtype=np.float)
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
 
-# actualicación 3.7 Para codificar datos categóricos y algunas transformaciones adicionales que aprenderéis en las próximas clases, en caso de que utilicéis la última versión de Python, veréis que ha cambiado un poco la librería y su sintaxis.
-
-###Aquí te dejo las nuevas versiones del código:
-
-##Para codificar datos categóricos ahora se utiliza:
-
-#from sklearn import preprocessing
-# le_X = preprocessing.LabelEncoder()
-# X[:,0] = le_X.fit_transform(X[:,0])
 #Para utilizar one hot encoder y crear variables dummy, ya no hace falta utilizar previamente la función label enconder, si no que para aplicar la dummyficación a la primera columna y dejar el resto de columnas como están, lo podemos hacer con:
 #
 #from sklearn.preprocessing import OneHotEncoder
 #from sklearn.compose import make_column_transformer
 #onehotencoder = make_column_transformer((OneHotEncoder(), [0]), remainder = "passthrough")
 #X = onehotencoder.fit_transform(X)
-#Para hacer reemplazo de valores por la media, ahora se utiliza:
-#
-#from sklearn.impute import SimpleImputer
-# Reemplazar por medias
-#imputer = SimpleImputer(strategy="mean")
-#medias en columnas 1,2
-#imputer = imputer.fit(X[:, 1:3])
-# Cambiar valores por dichas nedias
-#X[:, 1:3] = imputer.transform(X[:, 1:3])
+
 
 
 #Cambios de validación cruzada y training/testing
